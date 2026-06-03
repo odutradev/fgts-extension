b1f25d4e2d75ed5a895e767a8e61c5218baa9613
odutradev <joao.vitornl@gmail.com>
2026-06-03 11:46:59 -0300

    style: Reduces visual intensity of the grid and glow in gridbackground

--- src/popup/components/gridBackground/styles.ts
+++ src/popup/components/gridBackground/styles.ts
@@ -1,7 +1,7 @@
 import { Box, styled } from '@mui/material'
 
 export const BackgroundWrapper = styled(Box)(({ theme }) => {
-  const gridColor = theme.palette.mode === 'light' ? 'rgba(0, 0, 0, 0.05)' : 'rgba(255, 255, 255, 0.05)'
+  const gridColor = theme.palette.mode === 'light' ? 'rgba(0, 0, 0, 0.03)' : 'rgba(255, 255, 255, 0.03)'
 
   return {
     backgroundImage: `linear-gradient(${gridColor} 1px, transparent 1px), linear-gradient(90deg, ${gridColor} 1px, transparent 1px)`,
@@ -16,12 +16,12 @@ export const BackgroundWrapper = styled(Box)(({ theme }) => {
 })
 
 export const GlowOverlay = styled(Box)(({ theme }) => {
-  const glowColor = theme.palette.mode === 'light' ? 'rgba(0, 0, 0, 0.15)' : 'rgba(255, 255, 255, 0.15)'
+  const glowColor = theme.palette.mode === 'light' ? 'rgba(0, 0, 0, 0.07)' : 'rgba(255, 255, 255, 0.07)'
 
   return {
-    WebkitMaskImage: 'radial-gradient(circle 200px at var(--mouse-x, 0) var(--mouse-y, 0), black, transparent)',
+    WebkitMaskImage: 'radial-gradient(circle 140px at var(--mouse-x, 0) var(--mouse-y, 0), black, transparent)',
     backgroundImage: `linear-gradient(${glowColor} 1px, transparent 1px), linear-gradient(90deg, ${glowColor} 1px, transparent 1px)`,
-    maskImage: 'radial-gradient(circle 200px at var(--mouse-x, 0) var(--mouse-y, 0), black, transparent)',
+    maskImage: 'radial-gradient(circle 140px at var(--mouse-x, 0) var(--mouse-y, 0), black, transparent)',
     backgroundPosition: 'center',
     backgroundSize: '20px 20px',
     pointerEvents: 'none',
